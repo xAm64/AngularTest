54b76c838a938527733c6bd472462b2d816219f4
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-02-29 11:45:35 +0100

    ajout panier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainingsComponent } from './components/trainings/trainings.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
-  {path: "trainings", component: TrainingsComponent}
+  {path: "trainings", component: TrainingsComponent},
+  {path: "cart", component: CartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrainingsComponent } from './components/trainings/trainings.component';
 import { FormsModule } from '@angular/forms';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingsComponent
+    TrainingsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
